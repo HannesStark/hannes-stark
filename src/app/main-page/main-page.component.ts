1f25e83c172574f4c64f3b525cb2e3198b06ecf5
Hannes Stärk <hannes.staerk@gmail.com>
2020-10-25 17:03:09 +0100

    switch to sccs and almost get layout tdone

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -5,7 +5,7 @@ import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
   github = faGithub;
