b422c23ad16a8ca492239aae32f9522b31f80976
Hannes Stärk <hannes.staerk@gmail.com>
2020-12-13 11:43:16 +0100

    work on reviews

--- src/app/reviews/reviews.component.ts
+++ src/app/reviews/reviews.component.ts
@@ -1,17 +1,35 @@
 import {Component, OnInit} from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-reviews',
   templateUrl: './reviews.component.html',
-  styleUrls: ['./reviews.component.scss']
+  styleUrls: ['./reviews.component.scss'],
+  animations: [
+    trigger('openClose', [
+      // ...
+      state('closed', style({
+        height: '0px',
+        opacity: 0
+      })),
+      state('open', style({
+        opacity: 1
+      })),
+      transition('* => closed', [
+        animate('0.2s')
+      ]),
+      transition('* => open', [
+        animate('0.2s')
+      ]),
+    ]),
+  ],
 })
 export class ReviewsComponent implements OnInit {
-  collapsed = true;
+  open = false;
 
   constructor() {
   }
 
   ngOnInit(): void {
   }
-
 }
