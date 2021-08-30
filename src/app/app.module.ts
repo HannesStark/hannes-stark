50f3230e815d2a02d096a683e38afb9aa89b0906
Hannes Stärk <hannes.staerk@gmail.com>
2021-08-30 07:26:16 +0200

    nwe stuf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule, NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {MatSliderModule} from '@angular/material/slider';
 import {MainPageComponent} from './main-page/main-page.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
@@ -13,21 +13,21 @@ import {FlexLayoutModule} from '@angular/flex-layout';
 import {MatCardModule} from '@angular/material/card';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {MatListModule} from '@angular/material/list';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { ReviewsComponent } from './reviews/reviews.component';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {ReviewsComponent} from './reviews/reviews.component';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {HttpClientModule} from '@angular/common/http';
-import { FooterComponent } from './footer/footer.component';
+import {FooterComponent} from './footer/footer.component';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatDialogModule} from '@angular/material/dialog';
-import { LAImageComponent } from './laimage/laimage.component';
-import { ReadingGroupComponent } from './reading-group/reading-group.component';
+import {LAImageComponent} from './laimage/laimage.component';
+import {ReadingGroupComponent} from './reading-group/reading-group.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
-
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
 
 
 @NgModule({
@@ -59,8 +59,16 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     MatGridListModule,
     MatTooltipModule,
     MatDialogModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NoopAnimationsModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/reading-group/reading-group.component.ts
+++ src/app/reading-group/reading-group.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {faGithub, faLinkedin, faTwitter} from '@fortawesome/free-brands-svg-icons';
 import {faCopy, faEnvelope, faGraduationCap, faMapMarker, faMobile, faUser} from '@fortawesome/free-solid-svg-icons';
 import {faCalendar} from '@fortawesome/free-solid-svg-icons/faCalendar';
+import {FormBuilder, FormGroup, FormControl, Validators} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-reading-group',
@@ -20,10 +22,63 @@ export class ReadingGroupComponent implements OnInit {
   location = faMapMarker;
   paper = faCopy;
   mobile = faMobile;
-  constructor() {
+  form: FormGroup;
+  name: FormControl = new FormControl('', []);
+  email: FormControl = new FormControl('', [Validators.required, Validators.email]);
+  message: FormControl = new FormControl('', []);
+  honeypot: FormControl = new FormControl(''); // we will use this to prevent spam
+  success: boolean = false; // show and hide the success message
+  submit: boolean = false; // show and hide the success message
+  isLoading: boolean = false; // disable the submit button if we're loading
+  responseMessage: string; // the response message to show to the user
+
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
+    this.form = this.formBuilder.group({
+      name: this.name,
+      email: this.email,
+      message: this.message,
+      honeypot: this.honeypot
+    });
   }
 
   ngOnInit(): void {
   }
 
+  onSubmit() {
+    if (this.form.status == 'VALID' && this.honeypot.value == '') {
+      this.form.disable(); // disable the form if it's valid to disable multiple submissions
+      var formData: any = new FormData();
+      formData.append('name', this.form.get('name').value);
+      formData.append('email', this.form.get('email').value);
+      //formData.append('message', this.form.get('message').value);
+      this.isLoading = true; // sending the post request async so it's in progress
+      this.submit = false;
+      this.http.post('https://script.google.com/macros/s/AKfycbwL1PlWxp1Pd-l2HTPUJKZVsfNni5QkcH0QEDOb6hAHgsw59AdKmlLb9ZAONp8wmEaDig/exec', formData).subscribe(
+        (response) => {
+          // choose the response message
+          if (response['result'] == 'success') {
+            this.responseMessage = 'Success! I\'ll send you all updates!';
+            this.success = true; // show the response message
+          } else {
+            this.responseMessage = 'Sorry, something went wrong. Please reload and try again or directly send me your information to hannes.staerk@gmail.com.';
+            this.success = false;
+          }
+          this.form.enable(); // re enable the form after a success
+          this.isLoading = false; // re enable the submit button
+          console.log(response);
+        },
+        (error) => {
+          this.responseMessage = 'Sorry, an error occurred. Please reload and try again or directly send me your information to hannes.staerk@gmail.com.';
+          this.form.enable(); // re enable the form after a success
+          this.success = false; // show the response message
+          this.isLoading = false; // re enable the submit button
+          console.log(error);
+        }
+      );
+    } else {
+      this.success = false;
+      this.responseMessage = 'It appears that your email is not valid. Please check it.';
+    }
+    this.submit = true;
+  }
 }
